ff7fe9056fa3efec1b9e5e54e00b051bc6681c90
theryanclausen <theryanclausen@gmail.com>
2023-04-19 21:14:19 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import PersistentDrawerLeft from "./PermDrawer";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <PersistentDrawerLeft />
+    </>
+  );
+}
+
+export default App;
